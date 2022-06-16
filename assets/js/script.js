e54a155d5fea2edb20e30070ad8f6de1996ac70b
Becca Doan <rebecca.doan@cirrus.com>
2022-06-16 16:46:35 -0500

    finished adding storage of tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,17 @@
 var showDateEl = document.querySelector("#currentDay");
-
-var allTimesEl = document.querySelector("#all-times")
-
+var allTimesEl = document.querySelector("#all-times");
 
 
-$(".daily-schedule").on("click",".time-color",function(){
-    // var text = $(this).text().trim();
-    var taskInput = $(this).find("textarea");
+var schedule = {};
 
-    // var textInput = $("<textarea>").addClass("form-control").val(text);
-    // $(this).replaceWith(textInput);
-    // textInput.trigger("focus");
-})
-
-
+$(".time-save").on("click", function(){
+    // var text = allTimesEl.querySelector(".time-color").querySelector("textarea");
+    // var textEl =  $(this)[0].parentElement.querySelector(".time-color").querySelector("textarea");
+    var textEl =  $(this)[0].parentElement.querySelector(".time-color").getAttribute("id").replace("t-","");
+    // var text = $(textEl).val();
+    // console.log(text);
+    saveTask(textEl);
+  })
 
 var updateColors = function() {
     // put the current day info in the element to show the date.
@@ -24,18 +22,21 @@ var updateColors = function() {
     var currentHour = parseInt(moment().format("H"));
 
     // loop through all children of the time container
-    $(allTimesEl).children().each(function(){
-        
-        var timeSlot = parseInt($(this).children()[1].getAttribute("id"));
-        console.log(timeSlot);
+    $(allTimesEl).children().each(function() {
+        // grab the time data from the time slot
+        var timeSlot = parseInt($(this).children()[1].getAttribute("id").replace("t-",""));
+
+        // grey if time is complete
         if (timeSlot < currentHour) {
             $(this).find(".time-color").removeClass("green")
             $(this).find(".time-color").removeClass("red")
             $(this).find(".time-color").addClass("grey")
+        // red if time is current
         } else if (timeSlot === currentHour) {
             $(this).find(".time-color").removeClass("green")
             $(this).find(".time-color").removeClass("grey")
             $(this).find(".time-color").addClass("red")
+        // green if time has not yet happened
         } else {
             $(this).find(".time-color").removeClass("grey")
             $(this).find(".time-color").removeClass("red")
@@ -44,7 +45,43 @@ var updateColors = function() {
     })
 }
 
+var saveTask = function(id) {
+    var element = document. querySelector('#'+'t-'+id);
+    text = element.querySelector("textarea");
+    text = $(text).val();
+    console.log(text);
+    schedule[id] = text;
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+var loadSchedule = function() {
+    schedule = JSON.parse(localStorage.getItem("schedule"));
+
+    if (!schedule) {
+        schedule = {
+            9: "",
+            10: "",
+            11: "",
+            12: "",
+            13: "",
+            14: "",
+            15: "",
+            16: "",
+            17: ""
+        }
+    }
+
+    $(allTimesEl).children().each(function() {
+        var timeSlot = $(this).children()[1].getAttribute("id").replace("t-","");
+        var textValue = $(this).find("textarea");
+        console.log(schedule);
+        $(textValue).val(schedule[timeSlot]);
+    })
+}
+
+loadSchedule();
 updateColors();
+
 
 // update colors every minute
 setInterval(function () {
