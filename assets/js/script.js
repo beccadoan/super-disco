36592aa80e9cddf4fdd1810d793bd1faeb5c2b0b
Becca Doan <rebecca.doan@cirrus.com>
2022-06-16 13:57:25 -0500

    finished function that color coordinates the time based on the current local time

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var allTimesEl = document.querySelector("#all-times")
 
 $(".daily-schedule").on("click",".time-color",function(){
     // var text = $(this).text().trim();
+    var taskInput = $(this).find("textarea");
+
     // var textInput = $("<textarea>").addClass("form-control").val(text);
     // $(this).replaceWith(textInput);
     // textInput.trigger("focus");
@@ -24,9 +26,27 @@ var updateColors = function() {
     // loop through all children of the time container
     $(allTimesEl).children().each(function(){
         
-        $(this).children()[1].getAttribute("id");
-        $(this).find(".time-color").addClass("green")
+        var timeSlot = parseInt($(this).children()[1].getAttribute("id"));
+        console.log(timeSlot);
+        if (timeSlot < currentHour) {
+            $(this).find(".time-color").removeClass("green")
+            $(this).find(".time-color").removeClass("red")
+            $(this).find(".time-color").addClass("grey")
+        } else if (timeSlot === currentHour) {
+            $(this).find(".time-color").removeClass("green")
+            $(this).find(".time-color").removeClass("grey")
+            $(this).find(".time-color").addClass("red")
+        } else {
+            $(this).find(".time-color").removeClass("grey")
+            $(this).find(".time-color").removeClass("red")
+            $(this).find(".time-color").addClass("green")
+        }
     })
 }
 
 updateColors();
+
+// update colors every minute
+setInterval(function () {
+    updateColors()
+}, 60000);
